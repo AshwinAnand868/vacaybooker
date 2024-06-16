9f1e80488863e02a367db5a42bc0d11b2eeed200
AshwinAnand868 <anandashwin868@gmail.com>
2024-06-15 23:36:32 -0300

    Update the design of search box in the navbar

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,3 +1,5 @@
+import { BiSearch } from "react-icons/bi";
+
 const Search = () => {
   return (
     <div
@@ -28,6 +30,41 @@ const Search = () => {
             "
         >
           Anywhere
+        </div>
+        <div
+          className="
+            hidden
+            sm:block 
+            text-sm 
+            font-semibold 
+            px-6 
+            border-x-[1px] 
+            flex-1 
+            text-center
+          "
+        >
+          Any Week
+        </div>
+        <div className="
+          text-sm
+          pl-6
+          pr-2
+          text-gray-600
+          flex
+          flex-row
+          items-center
+          gap-3
+        ">
+          <div className="hidden sm:block">Add Guests</div>
+          <div
+            className="
+              p-2
+              bg-rose-500
+              rounded-full
+              text-white
+            ">
+              <BiSearch size={18}/>
+          </div>
         </div>
       </div>
     </div>
