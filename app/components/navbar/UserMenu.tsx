49c4781193af92efc418dfd967629e2b5510715f
AshwinAnand868 <anandashwin868@gmail.com>
2024-06-17 22:32:00 -0300

    Finish the basic Navbar UI part along with a simple modal

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,14 +1,23 @@
 'use client';
 
+import { useCallback, useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
+import MenuItem from "./MenuItem";
 
 const UserMenu = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleOpen = useCallback(() => {
+        setIsOpen((value) => !value);
+    }, [])
+
   return (
     <div className="relative">
         <div className="flex flex-row items-center gap-3">
             <div
-                onClick={() => {}}
+                onClick={()=> {}}
                 className="
                     hidden
                     md:block
@@ -22,10 +31,10 @@ const UserMenu = () => {
                     cursor-pointer
                 "
             >
-                Rent your home
+                Rent out your home
             </div>
             <div
-                onClick={() => {}}
+                onClick={toggleOpen}
                 className="
                     p-4
                     md:py-1
@@ -47,6 +56,36 @@ const UserMenu = () => {
                 </div>
             </div>
         </div>
+
+        {isOpen && (
+            <div
+                className="
+                    absolute
+                    right-0
+                    top-12
+                    text-sm
+                    bg-white
+                    overflow-hidden
+                    shadow-md
+                    w-[40vw]
+                    md:w-3/4
+                    rounded-xl
+                "
+            >
+                <div className="flex flex-col cursor-pointer">
+                    <>
+                        <MenuItem
+                            onClick={() => {}}
+                            label="Login"
+                        />
+                        <MenuItem
+                            onClick={() => {}}
+                            label="Sign up"
+                        />
+                    </>
+                </div>
+            </div>
+        )}
     </div>
   )
 }
