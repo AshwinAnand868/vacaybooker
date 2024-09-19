19d8a408d583ab2a4e3111062deec1a2b6e410d6
AshwinAnand868 <anandashwin868@gmail.com>
2024-09-18 22:47:21 -0400

    Set up cloudinary and enable image upload in rent modal

--- app/components/inputs/ImageUpload.tsx
+++ app/components/inputs/ImageUpload.tsx
@@ -1,9 +1,84 @@
 'use client';
 
+import { CldUploadWidget } from "next-cloudinary";
+import Image from "next/image";
+import { useCallback } from "react";
+import { TbPhotoPlus } from "react-icons/tb";
 
-const ImageUpload = () => {
+declare global {
+  var cloudinary: any;
+}
+
+interface ImageUploadProps {
+  onChange: (value: string) => void;
+  value: string;
+}
+
+const ImageUpload = ({
+  onChange,
+  value
+}: ImageUploadProps) => {
+
+
+  const handleUpload = useCallback((result: any) => {
+    onChange(result.info.secure_url);
+  }, [onChange]);
+
   return (
-    <div>ImageUpload</div>
+    <div>
+      <CldUploadWidget
+        onSuccess={handleUpload}
+        uploadPreset="fthyx5v5"
+        options={{
+          maxFiles: 1
+        }}
+      >
+
+        {({open}) => { 
+          // open variable exists but sometimes it is not available when the modal is open, so we will keep a check to save ourselves
+          return (
+            <div
+              onClick={() => open?.()}
+              className="
+                relative
+                cursor-pointer
+                hover:opacity-70
+                transition
+                border-dashed
+                border-2
+                border-neutral-300
+                text-neutral-600
+                p-20
+                flex
+                flex-col
+                justify-center
+                items-center
+                gap-4
+              "
+            >
+              <TbPhotoPlus size={50}/>
+              <div className="font-semibold text-lg">
+                Click to upload
+              </div>
+              {
+                value && (
+                  <div
+                    className="absolute inset-0 w-full h-full"
+                  >
+                    <Image
+                      alt="Upload"
+                      fill
+                      style={{ objectFit: 'cover' }}
+                      src={value}
+                    />
+                  </div>
+                )
+              }
+            </div>
+          )
+        }}
+      </CldUploadWidget>
+    </div>
   )
 }
 
