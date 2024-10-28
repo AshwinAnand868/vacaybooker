b1e8912005425e1e418429daf78191c77fe68276
AshwinAnand868 <anandashwin868@gmail.com>
2024-10-27 23:11:39 -0400

    Update stripe session object creation to include metadata

--- app/actions/stripe.ts
+++ app/actions/stripe.ts
@@ -14,7 +14,8 @@ export default async function createCheckoutSession(data: StripeData) {
                 unit_amount: formatAmountForStripe(data.totalPrice, 'usd'),
                 product_data: {
                     name: data.name,
-                    images: [data.image]
+                    images: [data.image],
+                    description: data.description,
                 }
             },
             quantity: 1,
@@ -24,11 +25,18 @@ export default async function createCheckoutSession(data: StripeData) {
     const checkoutSession = await stripe.checkout.sessions.create({
         payment_method_types: ["card"],
         line_items: lineItems,
+        metadata: {
+            totalPrice: data.totalPrice,
+            startDate: data.startDate,
+            endDate: data.endDate,
+            listingId: data.listingId,
+            userId: data.userId
+        },
         mode: "payment",
         submit_type: "book",
         success_url:
-        `${headers().get('origin')}/rent/item/checkout/result?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${headers().get('origin')}`,
+        `${headers().get('origin')}/listings/checkout/result?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${headers().get('origin')}/listings/${data.listingId}`,
     });
 
     redirect(checkoutSession.url as string);
